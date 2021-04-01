a7b5d09bd0c4811b0b00026d1ab1a375d2f40859
Khoa Dang Hoang <konadagoha@gmail.com>
2021-03-31 20:18:59 -0400

    add sample program

---
+++ Hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace km_web_scraper
+{
+    public class Hello
+    {
+        public int i = 345;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(new Hello().i.ToString());
+            int u = 100;
+        }
+    }
+}
