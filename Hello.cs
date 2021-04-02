26ce94a33fc813c384b85d53a714f9f12ebe5a4b
Khoa Dang Hoang <konadagoha@gmail.com>
2021-04-01 20:24:07 -0400

    Add first attempt in Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -1,15 +1,35 @@
 using System;
+using System.Net.Http;
+using System.Linq;
+using HtmlAgilityPack;
 
 namespace km_web_scraper
 {
-    public class Hello
+    public class Program
     {
-        public int i = 345;
-
         public static void Main(string[] args)
         {
-            Console.WriteLine(new Hello().i.ToString());
-            int u = 100;
+            GetHtmlAsync();
+    
+            Console.ReadLine();
+        }
+
+        private static async void GetHtmlAsync()
+        {
+            string url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=sword&_sacat=0";
+
+            HttpClient httpClient = new HttpClient();
+            string html = await httpClient.GetStringAsync(url);
+
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var productList = htmlDocument.DocumentNode.Descendants("ul")
+                .Where(node => node.GetAttributeValue("class", "id")
+                .Equals("srp-results srp-list clearfix")).ToList();
+
+            Console.WriteLine();
+
         }
     }
 }
